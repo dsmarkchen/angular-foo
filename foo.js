65e89f0d6b7e56a073402b014bd3e87c37052f20
Mark Chen <mchen@uts-canada.com>
2019-06-07 17:03:07 -0600

    more

--- foo.js
+++ foo.js
@@ -1,6 +1,9 @@
-var app = angular.module('fooApp', []);
-app.controller('fooCtrl', function($scope, $http) {
-    
+var foo_app = angular.module('fooApp', []);
+foo_app.controller('fooCtrl', function($scope, $http) {
+  $scope.myWelcome = "";
+  $scope.myGuideToStartL1="$mpm install -g local-web-server";
+  $scope.myGuideToStartL2="$ws";
+  
   $scope.cars = [
   {model : "Ford Mustang", color : "red"},
   {model : "Fiat 500", color : "white"},
@@ -20,5 +23,10 @@ app.controller('fooCtrl', function($scope, $http) {
   }, function myError(response) {
     $scope.myWelcome = response.statusText;
   });
+
+  $http.get("customers.php").then(function (response) {
+      $scope.myData = response.data.records;
+  });
+
 });
 
